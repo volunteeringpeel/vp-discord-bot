9cbe1bf997dedc2827f5de76cc17a888fe185a97
QiLinXue <645367@pdsb.net>
2018-11-27 21:09:23 -0500

    move imports, +function to update user info

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
-# 
 # VP Discord Bot
-#
+# pylint: disable=W0614
 
-# Discord Imports
-from discord.ext.commands import Bot
-import os
+from bot_setup import *
+from commands.random import *
+from commands.role_update import *
 
-# Load Bot
-from dotenv import load_dotenv
-load_dotenv(verbose=True)
-
-# Initialize Client
-client = Bot('')
-
+print("Starting up...")
 TOKEN = os.getenv('TOKEN')
 
-# -------------------------------
-# ------- Initialization --------
-# -------------------------------
-
-print("Starting up...") # Notify file was run
-
-# Notify if Bot was setup correctly
 @client.event
 async def on_ready():
     print("Bot is online")
 
-# -------------------------------
-# -------- Functions ------------
-# -------------------------------
-
 @client.event
 async def on_message(message):
-    if message.content.startswith("!ping"):
-        await client.send_message(message.channel, ":ping_pong: pong!")
+    '''
+    The following code updates basic info for all users on server
+        user.id (str): the user's id number
+        user.name (str): the user's username
+        channel.id (str): the id of the channel the bot uses to dm the user 
+    '''
+    if message.content == "update user info":
+        all_users = message.server.members
+        for user in all_users:
+            if not user.bot:
+                dm_channel = await client.start_private_message(user)
+                database.insert(user.id,user.name,str(dm_channel.id))
+
+        await client.send_message(message.channel, "completed")
+
+    '''
+    Processes all commands. Commands are found in the commands/ folder.
+    '''
+    await client.process_commands(message)
 
 # Run the Bot
 client.run(TOKEN)
